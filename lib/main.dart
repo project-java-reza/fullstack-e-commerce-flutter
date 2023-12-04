a50610ad2674062f9618481320b44daec2fd3237
project-java-reza <rizqirezaardianyahjava@gmail.com>
2023-12-05 06:54:42 +0700

    [FEATURE] Login Register Template

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce_fic9_new_build/presentation/auth/splash_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const SplashPage(),
+    );
+  }
+}
