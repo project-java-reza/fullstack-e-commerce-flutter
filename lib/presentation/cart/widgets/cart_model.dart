2efa3d03b2fa774c84ee3ad05254c572118a83d2
project-java-reza <rizqirezaardianyahjava@gmail.com>
2023-12-06 13:40:36 +0700

    [IMPROVE] remove attributes name and price

--- lib/presentation/cart/widgets/cart_model.dart
+++ lib/presentation/cart/widgets/cart_model.dart
@@ -1,19 +1,15 @@
-import 'package:flutter_ecommerce_fic9_new_build/common/extensions/int_ext.dart';
+import 'package:flutter_ecommerce_fic9_new_build/common/extensions/string_ext.dart';
+
+import '../../../data/models/responses/products_response_model.dart';
 
 class CartModel {
-  final int id;
-  final String imagePath;
-  final String name;
-  final int price;
+  final Product product;
   int quantity;
 
   CartModel({
-    required this.id,
-    required this.imagePath,
-    required this.name,
-    required this.price,
+    required this.product,
     this.quantity = 0,
   });
 
-  String get priceFormat => price.currencyFormatRp;
+  String get priceFormat => product.attributes.price.currencyFormatRp;
 }
