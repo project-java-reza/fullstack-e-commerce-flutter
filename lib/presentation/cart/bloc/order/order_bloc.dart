b75a5f2c5e291d4d750450ea4ee4106322127acb
project-java-reza <rizqirezaardianyahjava@gmail.com>
2023-12-06 15:09:11 +0700

    [FEATURE] implement bloc for order and freezed

--- lib/presentation/cart/bloc/order/order_bloc.dart
+++ lib/presentation/cart/bloc/order/order_bloc.dart
@@ -1,4 +1,6 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter_ecommerce_fic9_new_build/data/models/requests/order_request_model.dart';
+import 'package:flutter_ecommerce_fic9_new_build/data/models/responses/order_response_model.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'order_event.dart';
