1463451fd19d0cf2e2168f607c64f4319c63920a
project-java-reza <rizqirezaardianyahjava@gmail.com>
2023-12-08 15:54:15 +0700

    [FEATURE] add function getOrderById

--- lib/data/datasources/order_remote_datasource.dart
+++ lib/data/datasources/order_remote_datasource.dart
@@ -5,6 +5,8 @@ import 'package:flutter_ecommerce_fic9_new_build/data/models/requests/order_requ
 import 'package:flutter_ecommerce_fic9_new_build/data/models/responses/order_response_model.dart';
 import 'package:http/http.dart' as http;
 
+import '../models/responses/order_detail_response.dart';
+
 class OrderRemoteDataSource {
   // return nya diambil dari dart yaitu Either dan kalau gagal returnnya dalam bentuk String
   // kalau sukses returnnya dalam bentuk OrderResponseModel
@@ -23,6 +25,24 @@ class OrderRemoteDataSource {
 
     if (response.statusCode == 200) {
       return right(OrderResponseModel.fromJson(response.body));
+    } else {
+      return left('Server Error');
+    }
+  }
+
+  Future<Either<String, OrderDetailResponseModel>> getOrderById(
+      String id) async {
+    final token = await AuthLocalDataSource().getToken();
+    final response = await http.get(
+      Uri.parse('${Variables.baseUrl}/api/orders/$id'),
+      headers: <String, String>{
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      return right(OrderDetailResponseModel.fromJson(response.body));
     } else {
       return left('Server Error');
     }
