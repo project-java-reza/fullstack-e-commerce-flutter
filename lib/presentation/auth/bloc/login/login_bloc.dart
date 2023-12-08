c01361d7b56c21435d9b7913c6d457f34abaf05a
project-java-reza <rizqirezaardianyahjava@gmail.com>
2023-12-08 12:46:57 +0700

    [IMPROVE]change name file login request model

--- lib/presentation/auth/bloc/login/login_bloc.dart
+++ lib/presentation/auth/bloc/login/login_bloc.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_ecommerce_fic9_new_build/data/datasources/auth_remote_datasource.dart';
-import 'package:flutter_ecommerce_fic9_new_build/data/models/requests/login_request_mode.dart';
+import 'package:flutter_ecommerce_fic9_new_build/data/models/requests/login_request_model.dart';
 import 'package:flutter_ecommerce_fic9_new_build/data/models/responses/auth_response_model.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
