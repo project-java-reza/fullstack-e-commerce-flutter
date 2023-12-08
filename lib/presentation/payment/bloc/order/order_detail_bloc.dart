2440ab370d7e9864e7d5e1c4f642b3b101c067b0
project-java-reza <rizqirezaardianyahjava@gmail.com>
2023-12-08 15:59:36 +0700

    [FEATURE] bloc and freezed for order detail

---
+++ lib/presentation/payment/bloc/order/order_detail_bloc.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_ecommerce_fic9_new_build/data/datasources/order_remote_datasource.dart';
+import 'package:flutter_ecommerce_fic9_new_build/data/models/responses/order_detail_response.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'order_detail_event.dart';
+part 'order_detail_state.dart';
+part 'order_detail_bloc.freezed.dart';
+
+class OrderDetailBloc extends Bloc<OrderDetailEvent, OrderDetailState> {
+  OrderDetailBloc() : super(const _Initial()) {
+    on<_GetOrderDetail>((event, emit) async {
+      emit(const _Loading());
+      final response =
+          await OrderRemoteDataSource().getOrderById(event.orderId);
+      response.fold(
+        (l) => emit(_Error(l)),
+        (r) => emit(_Success(r)),
+      );
+    });
+  }
+}
