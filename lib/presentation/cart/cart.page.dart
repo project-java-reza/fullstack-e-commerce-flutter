1e0d53cbb6061577f9f487fa9c7be429a36158bc
project-java-reza <rizqirezaardianyahjava@gmail.com>
2023-12-10 16:21:10 +0700

    [IMPROVE] BlocBuilder for GetAddress

--- lib/presentation/cart/cart.page.dart
+++ lib/presentation/cart/cart.page.dart
@@ -1,8 +1,12 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_ecommerce_fic9_new_build/common/extensions/int_ext.dart';
 import 'package:flutter_ecommerce_fic9_new_build/data/models/requests/order_request_model.dart';
+import 'package:flutter_ecommerce_fic9_new_build/presentation/cart/bloc/bloc/get_cost_bloc.dart';
 import 'package:flutter_ecommerce_fic9_new_build/presentation/payment/payment_page.dart';
+import 'package:flutter_ecommerce_fic9_new_build/presentation/shipping_address/bloc/get_address/get_address_bloc.dart';
 import '../../common/component/button.dart';
 import '../../common/component/row_text.dart';
 import '../../common/component/spaces.dart';
@@ -24,11 +28,14 @@ class CartPage extends StatefulWidget {
 class _CartPageState extends State<CartPage> {
   @override
   void initState() {
+    context.read<GetAddressBloc>().add(const GetAddressEvent.getAddress());
     super.initState();
   }
 
   List<Item> items = [];
   int localTotalPrice = 0;
+
+  int idAddress = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -64,12 +71,13 @@ class _CartPageState extends State<CartPage> {
             margin: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
             child: Button.filled(
               width: 60,
-              onPressed: () {
-                Navigator.push(
+              onPressed: () async {
+                idAddress = await Navigator.push(
                   context,
                   MaterialPageRoute(
                       builder: (context) => const ShippingAddressPage()),
                 );
+                setState(() {});
               },
               label: 'Pilih Alamat Pengiriman',
             ),
@@ -77,60 +85,88 @@ class _CartPageState extends State<CartPage> {
 
           const SpaceHeight(16.0),
           // show alamat pengiriman
-          Container(
-            padding: const EdgeInsets.all(16.0),
-            decoration: BoxDecoration(
-              borderRadius: const BorderRadius.all(Radius.circular(5.0)),
-              border: Border.all(color: ColorName.border),
-            ),
-            child: const Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Alamat Pengiriman',
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
-                SpaceHeight(16.0),
-                Text(
-                  'Nama Lengkap',
-                  style: TextStyle(
-                    fontSize: 12,
-                    fontWeight: FontWeight.w400,
-                    color: ColorName.grey,
-                  ),
-                ),
-                SpaceHeight(8.0),
-                Text(
-                  'Alamat Lengkap',
-                  style: TextStyle(
-                    fontSize: 12,
-                    fontWeight: FontWeight.w400,
-                    color: ColorName.grey,
-                  ),
-                ),
-                SpaceHeight(8.0),
-                Text(
-                  'Kota, Provinsi, Kode Pos',
-                  style: TextStyle(
-                    fontSize: 12,
-                    fontWeight: FontWeight.w400,
-                    color: ColorName.grey,
-                  ),
-                ),
-                SpaceHeight(8.0),
-                Text(
-                  'No Handphone',
-                  style: TextStyle(
-                    fontSize: 12,
-                    fontWeight: FontWeight.w400,
-                    color: ColorName.grey,
-                  ),
-                ),
-              ],
-            ),
+          BlocBuilder<GetAddressBloc, GetAddressState>(
+            builder: (context, state) {
+              return state.maybeWhen(
+                orElse: () {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                },
+                loaded: (data) {
+                  if (data.data.isEmpty) {
+                    return const Center(
+                      child: Text('Alamat belum tersedia'),
+                    );
+                  }
+                  // get address is default
+                  // final address = data.data.firstWhere(
+                  //   (element) => element.attributes.isDefault,
+                  //   orElse: () => data.data.first,
+                  // );
+                  final address = idAddress != 0
+                      ? data.data.firstWhere(
+                          (element) => element.id == idAddress,
+                          orElse: () => data.data.first,
+                        )
+                      : data.data.last;
+                  context.read<GetCostBloc>().add(
+                        GetCostEvent.getCost(
+                            origin: '6268',
+                            destination:
+                                address.attributes.subdistrictId.toString(),
+                            courier: 'jne'),
+                      );
+                  return Container(
+                    padding: const EdgeInsets.all(16.0),
+                    decoration: BoxDecoration(
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(5.0)),
+                      border: Border.all(color: ColorName.border),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Alamat Pengiriman',
+                          style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        const SpaceHeight(16.0),
+                        Text(
+                          address.attributes.name,
+                          style: const TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w400,
+                            color: ColorName.grey,
+                          ),
+                        ),
+                        const SpaceHeight(8.0),
+                        Text(
+                          address.attributes.address,
+                          style: const TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w400,
+                            color: ColorName.grey,
+                          ),
+                        ),
+                        const SpaceHeight(8.0),
+                        Text(
+                          address.attributes.phone,
+                          style: const TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w400,
+                            color: ColorName.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              );
+            },
           ),
           const SpaceHeight(16.0),
           //container for dropdown courier
@@ -192,9 +228,29 @@ class _CartPageState extends State<CartPage> {
                   },
                 ),
                 const SpaceHeight(12.0),
-                const RowText(
-                  label: 'Biaya Pengiriman',
-                  value: 'Free Ongkir', //150000.currencyFormatRp,
+                BlocBuilder<GetCostBloc, GetCostState>(
+                  builder: (context, state) {
+                    return state.maybeWhen(
+                      orElse: () {
+                        return RowText(
+                          label: 'Biaya Pengiriman',
+                          value: 0.currencyFormatRp,
+                        );
+                      },
+                      loading: () {
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      },
+                      loaded: (cost) {
+                        return RowText(
+                          label: 'Biaya Pengiriman',
+                          value: cost.rajaongkir.results.first.costs.first.cost
+                              .first.value.currencyFormatRp,
+                        );
+                      },
+                    );
+                  },
                 ),
                 const SpaceHeight(40.0),
                 const Divider(color: ColorName.border),
