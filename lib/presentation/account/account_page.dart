ddac1205fb43e5cfaf355069136f8535f251729c
project-java-reza <rizqirezaardianyahjava@gmail.com>
2023-12-11 14:33:27 +0700

    [FEATURE] account page

--- lib/presentation/account/account_page.dart
+++ lib/presentation/account/account_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_ecommerce_fic9_new_build/data/datasources/auth_local_datasource.dart';
-import 'package:flutter_ecommerce_fic9_new_build/presentation/auth/login_page.dart';
+import '../../common/constant/colors.dart';
+import '../../common/constant/images.dart';
+import '../../data/datasources/auth_local_datasource.dart';
+import '../auth/login_page.dart';
+import '../order/order_page.dart';
+import '../shipping_address/shipping_address_page.dart';
 
 class AccountPage extends StatefulWidget {
   const AccountPage({super.key});
@@ -13,17 +17,106 @@ class _AccountPageState extends State<AccountPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: ElevatedButton(
-        onPressed: () async {
-          await AuthLocalDataSource().removeAuthData();
-          Navigator.pushReplacement(context,
-              MaterialPageRoute(builder: (context) {
-            return const LoginPage();
-          }));
-        },
-        child: const Text('Logout'),
-      )),
+      appBar: AppBar(
+        title: const Text('Account'),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            leading: Image.asset(
+              Images.iconUser,
+              width: 24.0,
+            ),
+            title: const Text(
+              'Profil',
+              style: TextStyle(
+                color: ColorName.primary,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            onTap: () {
+              // Navigator.push(
+              //   context,
+              //   MaterialPageRoute(builder: (context) => const ProfilePage()),
+              // );
+            },
+          ),
+          ListTile(
+            leading: Image.asset(
+              Images.iconBag,
+              width: 24.0,
+            ),
+            title: const Text(
+              'Pesanan',
+              style: TextStyle(
+                color: ColorName.primary,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const OrderPage()),
+              );
+            },
+          ),
+          ListTile(
+            leading: Image.asset(
+              Images.iconLocation,
+              width: 24.0,
+            ),
+            title: const Text(
+              'Alamat',
+              style: TextStyle(
+                color: ColorName.primary,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const ShippingAddressPage()),
+              );
+            },
+          ),
+          ListTile(
+            leading: const Icon(
+              Icons.logout,
+              color: ColorName.primary,
+              size: 24.0,
+            ),
+            title: const Text(
+              'Logout',
+              style: TextStyle(
+                color: ColorName.primary,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            onTap: () async {
+              await AuthLocalDataSource().removeAuthData();
+              Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: (context) {
+                return const LoginPage();
+              }));
+            },
+          ),
+        ],
+      ),
     );
   }
 }
+
+// return Scaffold(
+//       body: Center(
+//           child: ElevatedButton(
+//         onPressed: () async {
+//           await AuthLocalDataSource().removeAuthData();
+//           Navigator.pushReplacement(context,
+//               MaterialPageRoute(builder: (context) {
+//             return const LoginPage();
+//           }));
+//         },
+//         child: const Text('Logout'),
+//       )),
+//     );
