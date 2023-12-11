b846a48365c08059ee1cef223f7ae8df4af7eca4
project-java-reza <rizqirezaardianyahjava@gmail.com>
2023-12-11 14:55:43 +0700

    [FEATURE] create field hardcode code courier and name courier and definition drop point delivery

---
+++ lib/presentation/cart/models/courier.dart
@@ -0,0 +1,22 @@
+class Courier {
+  final String code;
+  final String name;
+
+  Courier({
+    required this.code,
+    required this.name,
+  });
+
+  @override
+  String toString() => name;
+}
+
+List<Courier> couriers = [
+  Courier(code: 'jne', name: 'JNE'),
+  Courier(code: 'pos', name: 'POS'),
+  Courier(code: 'jnt', name: 'J&T'),
+];
+
+// kode subdistrict origin
+const String subdistrictOrigin = '6268'; 
+// pengaturan ini bisa ditaro di backend atau pengaturan config
